41f0539778ababd5dcb5513121daa2b956d81ab9
NguyenQuangVinh_24022760 <24022760@vnu.edu.vn>
2025-09-26 22:20:19 +0700

    Ham main

---
+++ Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    private ControlWindow cw = new ControlWindow();
+    public Main(){
+        this.setTitle("Akarnoid");
+        this.setSize(ControlWindow.WIDTH, ControlWindow.HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setVisible(true);
+    this.add(cw);
+    //this.pack();
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
